187bc5156ecb8083594464c5154b49cb2292875d
amemonkey <ameliafunaki789@gmail.com>
2018-06-08 14:55:59 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,19 +1,41 @@
-def simple_curfew_checker(time)
-  # code goes here
-end
+# put learn in terminal in the beginning to see what is wanted
+# def simple_curfew_checker(time)
+#   if time is == 11
+#     puts "You're in trouble! Better get home quick!"
+#     elsif time is > 11
+#     puts "You're in trouble! Better get home quick!"
+#   else time is < 11
+#     puts "nil" 
+# end
+# end
+
+# simple_curfew_checker(11)
 
 def curfew_checker(time)
-  # code goes here
+  if time is > 11
+    puts nil
+  else 
+    puts "You're in trouble!"
 end
 
-def complex_curfew_checker(time)
-  # code goes here
-end
+# def complex_curfew_checker(time)
+#   if 6 < 5
+#     puts "Six is less than five."
+#     elsif 6 == 6
+#     puts "Six equals six"
+#   else
+#     puts "Six is not less than five and six does not equal six"
+# end
 
-def deluxe_curfew_checker(time)
-  # code goes here
-end
+# def deluxe_curfew_checker(time)
+#   6 < 11
+#     puts "Six is less than eleven."
+#     puts "I have" + num_of_hours.to_s + "hours."
+#     puts "I have #{num_of_hours}
+#     hours."
+  
+# end
 
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+# def platinum_curfew_checker(current_time, curfew_time)
+#   # code goes here
+# end
